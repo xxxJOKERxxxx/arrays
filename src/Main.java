b832bf146dcea298b6a5019b113ab9e6d103f7f5
xxxJOKERxxx <0888200@gmail.com>
2025-12-25 23:35:32 +0300

    массив

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int number: array) {
+            System.out.println(number);
+        }
+        System.out.println("Hello, World!");
+    }
+}
