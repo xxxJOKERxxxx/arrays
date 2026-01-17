febe0094bff1f1cb51e44e64a191e034a9a0b082
xxxJOKERxxx <0888200@gmail.com>
2026-01-17 17:21:07 +0300

    тернарный опратор

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-      int a = 12;
-     String s = switch (a) {
-          case 1 -> "Hello";
-          default -> "World";
-      };
-     System.out.println(s);
 
-
+        int age = 20;
+        String info = age >=18 ? "adult" : "teen";
+        System.out.println(info);
     }
 }
