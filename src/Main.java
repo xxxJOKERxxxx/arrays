b0b471c47f71280fde4f1f05a322f7a50f0faabc
xxxJOKERxxx <0888200@gmail.com>
2026-01-17 19:16:50 +0300

    цикл FOREACH String

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array = new int[10];
-        for(int number : array) {
-            System.out.println(number);
+        String[] strArray = new String[5];
+
+        for(String str : strArray) {
+
+            System.out.println(str);
         }
 
 
