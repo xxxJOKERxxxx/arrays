007d3b891c6b62161919a472ee53824433cddf42
xxxJOKERxxx <0888200@gmail.com>
2026-01-17 14:04:10 +0300

    switch case

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-      int a = 15;
-      if (a > 10) {
-          a+=10;
-      } else if (a<10) {
-          a-=10;
-      } else a =100;
-      System.out.println(a);
+      int a = 10;
+      switch (a) {
+          case 1 -> System.out.println(1);
+          default -> System.out.println(2);
+      }
     }
 }
