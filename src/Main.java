378720636f29d38d1af50c3581fbf5552958bb8f
xxxJOKERxxx <0888200@gmail.com>
2026-01-17 13:32:02 +0300

    if else if

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       for (int i = 1; i <= 10; i++) {
-           if (i % 2 == 0) {
-               continue;
-           }
-           if (i > 7) {
-               break;
-           }
-           System.out.println(i);
-       }
-
+      int a = 15;
+      if (a > 10) {
+          a+=10;
+      } else if (a<10) {
+          a-=10;
+      } else a =100;
+      System.out.println(a);
     }
 }
