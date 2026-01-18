7f085f8831ee9076152935725a6d4f2109ca0cac
xxxJOKERxxx <0888200@gmail.com>
2026-01-18 14:16:06 +0300

    ОБЬЕКТЫ,КОНСТРУКТОР

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[][] array = new int [3][3];
-        for (int i = 0; i < array.length; i++) {
-            for (int j =0; j <array[i].length; j++) {
-                array[i][j] = i;
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
+        Person person1 = new Person();
+        Person person2 = new Person("Andrei");
 
-        }
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    String name;
+    int age;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+
+    }
+
+
+}
